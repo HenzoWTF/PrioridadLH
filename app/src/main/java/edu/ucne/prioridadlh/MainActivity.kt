cf22920edf1956741c4e4242f4f7df2b79edfab7
HenzoWTF <Urenahenzo@gmail.com>
2024-08-31 09:54:50 -0400

    OutlineTextField listo

--- app/src/main/java/edu/ucne/prioridadlh/MainActivity.kt
+++ app/src/main/java/edu/ucne/prioridadlh/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.room.Room
 import edu.ucne.prioridadlh.data.local.database.PrioridadDb
 import edu.ucne.prioridadlh.ui.theme.PrioridadLHTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private  lateinit var prioridadDb: PrioridadDb
@@ -38,6 +64,7 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(innerPadding)
                     ){
+                        PrioridadScreen()
 
                     }
                 }
@@ -46,11 +73,53 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun PrioridadScreen(){
+    var Descripcion by remember { mutableStateOf("") }
+    var DiasCompromiso by remember { mutableStateOf("") }
+    var Error: String? by remember { mutableStateOf(null) }
+
+    Scaffold{ innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(8.dp)
+        ) {
+            ElevatedCard(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    OutlinedTextField(
+                        label = { Text(text = "Descripción") },
+                        value = Descripcion,
+                        onValueChange = { Descripcion = it },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    OutlinedTextField(
+                        label = { Text(text = "Días Compromiso") },
+                        value = DiasCompromiso,
+                        onValueChange = { DiasCompromiso = it },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.padding(2.dp))
+//                    Error.let {
+//                        Text(text = it, color = Color.Red)
+//                    }
+                }
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     PrioridadLHTheme {
-
+        PrioridadScreen()
     }
 }

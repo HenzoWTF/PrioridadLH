43247a65cbe842e178784d8577246eed71872fb1
HenzoWTF <Urenahenzo@gmail.com>
2024-09-15 11:54:59 -0400

    Actualización de la clase prioridad

--- app/src/main/java/edu/ucne/prioridadlh/data/local/entities/PrioridadesEntity.kt
+++ app/src/main/java/edu/ucne/prioridadlh/data/local/entities/PrioridadesEntity.kt
@@ -2,11 +2,13 @@ package edu.ucne.prioridadlt.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = "Prioridades")
 data class PrioridadesEntity(
     @PrimaryKey
     val PrioridadId: Int? = null,
     val Descripcion: String = "",
-    val DiasCompromiso: String = ""
+    val DiasCompromiso: Int
 )

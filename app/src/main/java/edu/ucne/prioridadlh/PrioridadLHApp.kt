600c4f71c7eb4213a94d862db1711d9ccf805f2e
HenzoWTF <Urenahenzo@gmail.com>
2024-09-15 11:57:47 -0400

    Repository de prioridad listo

--- app/src/main/java/edu/ucne/prioridadlh/PrioridadLHApp.kt
+++ app/src/main/java/edu/ucne/prioridadlh/PrioridadLHApp.kt
@@ -1,14 +1,9 @@
 package edu.ucne.prioridadlh
 
-import edu.ucne.prioridadlh.data.local.dao.PrioridadesDao
-import edu.ucne.prioridadlt.data.local.entities.PrioridadesEntity
-import javax.inject.Inject
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-class PrioridadRepository @Inject constructor(
-    private val prioridadDao: PrioridadesDao)
-{
-    suspend fun save(prioridad: PrioridadesEntity) = prioridadDao.save(prioridad)
-    suspend fun delete(prioridad: PrioridadesEntity) = prioridadDao.delete(prioridad)
-    fun getAll() = prioridadDao.getAll()
-    suspend fun find(id: Int) = prioridadDao.find(id)
+@HiltAndroidApp
+class RegistroPrioridadesApp : Application() {
+
 }

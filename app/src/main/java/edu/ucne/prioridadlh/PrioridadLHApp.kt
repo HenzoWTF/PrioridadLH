ff59ef50ce651bcf3ce442883100e20383c73020
HenzoWTF <Urenahenzo@gmail.com>
2024-09-15 01:14:44 -0400

    PrioridadRepository listo

---
+++ app/src/main/java/edu/ucne/prioridadlh/PrioridadLHApp.kt
@@ -0,0 +1,14 @@
+package edu.ucne.prioridadlh
+
+import edu.ucne.prioridadlh.data.local.dao.PrioridadesDao
+import edu.ucne.prioridadlt.data.local.entities.PrioridadesEntity
+import javax.inject.Inject
+
+class PrioridadRepository @Inject constructor(
+    private val prioridadDao: PrioridadesDao)
+{
+    suspend fun save(prioridad: PrioridadesEntity) = prioridadDao.save(prioridad)
+    suspend fun delete(prioridad: PrioridadesEntity) = prioridadDao.delete(prioridad)
+    fun getAll() = prioridadDao.getAll()
+    suspend fun find(id: Int) = prioridadDao.find(id)
+}

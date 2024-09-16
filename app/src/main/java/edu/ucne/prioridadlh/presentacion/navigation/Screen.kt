7e9fd98b682d7f8f8b6743271a04931939defa51
HenzoWTF <Urenahenzo@gmail.com>
2024-09-16 00:14:33 -0400

    Screen para la navegacion listo con ticket

--- app/src/main/java/edu/ucne/prioridadlh/presentacion/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/prioridadlh/presentacion/navigation/Screen.kt
@@ -11,4 +11,13 @@ sealed class Screen(val route: String) {
             fun createRoute(id: Int) = "prioridad/$id"
         }
     }
+
+
+    object TicketList : Screen("ticket_list")
+
+    data class Ticket(val id: Int) : Screen("ticket/{ticketId}") {
+        companion object {
+            fun createRoute(id: Int) = "ticket/$id"
+        }
+    }
 }

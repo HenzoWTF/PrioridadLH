f2bb827814a6104d9c7fb3a740c6d674f80c5a41
HenzoWTF <Urenahenzo@gmail.com>
2024-09-16 00:11:55 -0400

    TicketRepository listo

---
+++ app/src/main/java/edu/ucne/prioridadlh/data/Repository/TicketRepository.kt
@@ -0,0 +1,18 @@
+package edu.ucne.prioridadlh.data.Repository
+
+import edu.ucne.prioridadlh.data.local.dao.TicketDao
+import edu.ucne.prioridadlh.data.local.entities.TicketEntity
+import javax.inject.Inject
+
+class TicketRepository @Inject constructor(
+    private val ticketDao: TicketDao
+) {
+    suspend fun save(ticket: TicketEntity) = ticketDao.save(ticket)
+    suspend fun delete(ticket: TicketEntity) = ticketDao.delete(ticket)
+    suspend fun finId(ticketId: Int) = ticketDao.find(ticketId)
+    suspend fun findCliente(cliente: String) = ticketDao.findCliente(cliente)
+    suspend fun findAsunto(asunto: String) = ticketDao.findAsunto(asunto)
+    suspend fun findDescripcion(descripcion: String) = ticketDao.findDescripcion(descripcion)
+    fun getTicket() =ticketDao.getAll()
+
+}

68647b6f37732053814254829e4a92816374c6b4
HenzoWTF <Urenahenzo@gmail.com>
2024-09-16 00:27:21 -0400

    TicketUiEvent Listo

---
+++ app/src/main/java/edu/ucne/prioridadlh/presentacion/ticket/TicketUiEvent.kt
@@ -0,0 +1,15 @@
+package edu.ucne.prioridadlh.presentacion.ticket
+
+import java.util.Date
+
+sealed interface TicketUiEvent {
+    data class TicketIdChanged(val ticketId: Int): TicketUiEvent
+    data class FecaChanged(val Fecha: Date?) : TicketUiEvent
+    data class ClienteChanged(val Cliente: String): TicketUiEvent
+    data class AsuntoChanged(val Asunto: String) : TicketUiEvent
+    data class DescripcionChanged(val Descripcion: String) : TicketUiEvent
+    data class PrioridadIdChanged(val PrioridadId: String) : TicketUiEvent
+    data class TicketSelected(val TicketId: Int) : TicketUiEvent
+    object  Save : TicketUiEvent
+    object  Delete : TicketUiEvent
+}

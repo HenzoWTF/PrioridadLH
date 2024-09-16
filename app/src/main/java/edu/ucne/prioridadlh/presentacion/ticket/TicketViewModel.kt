417e75dd31dc37ddfd70efe1770a642b0f9b5ab8
HenzoWTF <Urenahenzo@gmail.com>
2024-09-16 15:54:00 -0400

    TicketViewModel cambiado

--- app/src/main/java/edu/ucne/prioridadlh/presentacion/ticket/TicketViewModel.kt
+++ app/src/main/java/edu/ucne/prioridadlh/presentacion/ticket/TicketViewModel.kt
@@ -26,79 +26,194 @@ class TicketViewModel @Inject constructor(
         getPrioridades()
     }
 
-    private fun getTickets() {
+    fun onEvent(event: TicketUiEvent) {
+        when (event) {
+            is TicketUiEvent.FechaChange -> onFechaChanged(event.fecha)
+            is TicketUiEvent.PrioridadChange -> onPrioridadChanged(event.prioridadId)
+            is TicketUiEvent.ClienteChange -> onClienteChanged(event.cliente)
+            is TicketUiEvent.AsuntoChange -> onAsuntoChanged(event.asunto)
+            is TicketUiEvent.DescripcionChange -> onDescripcionChanged(event.descripcion)
+            TicketUiEvent.Save -> saveTicket()
+            is TicketUiEvent.Delete -> deleteTicket(event.ticketId)
+            TicketUiEvent.New -> newTicket()
+            TicketUiEvent.Validation -> _uiState.value.validation = validateInput()
+            is TicketUiEvent.SelectTicket -> SelectTicket(event.ticketId)
+        }
+    }
+
+    private fun saveTicket() {
         viewModelScope.launch {
-            ticketRepository.getTicket().collect { tickets ->
-                _uiState.update { it.copy(Tickets = tickets) }
+            ticketRepository.save(uiState.value.toEntity())
+            newTicket()
+        }
+    }
+
+    private fun deleteTicket(ticketId: Int) {
+        viewModelScope.launch {
+            ticketRepository.finId(ticketId).let { ticket ->
+                ticketRepository.delete(ticket)
+                newTicket()
             }
+        }
+    }
+
+    private fun newTicket() {
+        _uiState.update {
+            it.copy(
+                ticketId = null,
+                fecha = "",
+                prioridadId = null,
+                cliente = "",
+                asunto = "",
+                descripcion = "",
+                errorFecha = "",
+                errorPrioridad = "",
+                errorCliente = "",
+                errorAsunto = "",
+                errorDescripcion = "",
+                validation = false
+            )
         }
     }
 
     private fun getPrioridades() {
         viewModelScope.launch {
             prioridadRepository.getAll().collect { prioridades ->
-                _uiState.update { it.copy(Prioridades = prioridades) }
+                _uiState.update {
+                    it.copy(prioridades = prioridades)
+                }
             }
         }
     }
-
-    fun onEvent(event: TicketUiEvent) {
+    private fun getTickets() {
         viewModelScope.launch {
-            when (event) {
-                is TicketUiEvent.TicketIdChanged -> _uiState.update { it.copy(TicketId = event.ticketId) }
-                is TicketUiEvent.FechaChanged -> _uiState.update { it.copy(Fecha = event.Fecha) }
-                is TicketUiEvent.PrioridadIdChanged -> _uiState.update { it.copy(PrioridadId = event.PrioridadId.toInt()) }
-                is TicketUiEvent.ClienteChanged -> _uiState.update { it.copy(Cliente = event.Cliente) }
-                is TicketUiEvent.AsuntoChanged -> _uiState.update { it.copy(Asunto = event.Asunto) }
-                is TicketUiEvent.DescripcionChanged -> _uiState.update { it.copy(Descripcion = event.Descripcion) }
-                is TicketUiEvent.TicketSelected -> {
-                    if (event.TicketId > 0) {
-                        val ticket = ticketRepository.finId(event.TicketId)
-                        _uiState.update {
-                            it.copy(
-                                TicketId = ticket?.TicketId,
-                                Fecha = ticket?.Fecha,
-                                PrioridadId = ticket?.PrioridadId ?: 0,
-                                Cliente = ticket?.Cliente,
-                                Asunto = ticket?.Asunto,
-                                Descripcion = ticket?.Descripcion
-                            )
-                        }
-                    }
-                }
-                TicketUiEvent.Save -> {
-                    val errorMessage = validateInput()
-                    if (errorMessage == null) {
-                        ticketRepository.save(uiState.value.toEntity())
-                        _uiState.update { it.copy(success = true) }
-                    } else {
-                        _uiState.update { it.copy(errorMessage = errorMessage) }
-                    }
-                }
-                TicketUiEvent.Delete -> {
-                    ticketRepository.delete(uiState.value.toEntity())
+            ticketRepository.getTicket().collect { tickets ->
+                _uiState.update {
+                    it.copy(tickets = tickets)
                 }
             }
         }
     }
 
-    private fun validateInput(): String? {
-        return when {
-            _uiState.value.Cliente.isNullOrBlank() -> "El campo cliente no puede estar vacío."
-            _uiState.value.Asunto.isNullOrBlank() -> "El campo asunto no puede estar vacío."
-            _uiState.value.Fecha == null -> "El campo fecha no puede estar vacío."
-            _uiState.value.PrioridadId <= 0 -> "El campo prioridad no puede estar vacío."
-            _uiState.value.Descripcion.isNullOrBlank() -> "El campo descripción no puede estar vacío."
-            else -> null
+    private fun onPrioridadChanged(prioridadIdString: String) {
+        val prioridadId = prioridadIdString.toIntOrNull()
+        if (prioridadId != null) {
+            selectedPrioridad(prioridadId)
+        } else {
+            _uiState.update {
+                it.copy(
+                    prioridadId = null,
+                    errorPrioridad = "Prioridad inválida."
+                )
+            }
         }
     }
 
-    private fun TicketUiState.toEntity() = TicketEntity(
-        TicketId = TicketId,
-        Fecha = Fecha,
-        PrioridadId = PrioridadId,
-        Cliente = Cliente ?: "",
-        Asunto = Asunto ?: "",
-        Descripcion = Descripcion ?: ""
+    private fun selectedPrioridad(prioridadId: Int) {
+        viewModelScope.launch {
+            val prioridad = prioridadRepository.find(prioridadId)
+            _uiState.update {
+                it.copy(
+                    prioridadId = prioridad?.PrioridadId,
+                    errorPrioridad = if (prioridad == null) "Prioridad no encontrada." else ""
+                )
+            }
+        }
+    }
+    private fun SelectTicket(ticketId: Int) {
+        viewModelScope.launch {
+            val ticket = ticketRepository.finId(ticketId)
+            _uiState.update {
+                it.copy(
+                    ticketId = ticket?.TicketId,
+                    fecha = ticket?.fecha ?: "",
+                    prioridadId = ticket?.PrioridadId,
+                    cliente = ticket?.Cliente ?: "",
+                    asunto = ticket?.Asunto ?: "",
+                    descripcion = ticket?.Descripcion ?: "",
+                    errorFecha = "",
+                    errorPrioridad = "",
+                    errorCliente = "",
+                    errorAsunto = "",
+                    errorDescripcion = "",
+                    validation = false
+                )
+            }
+        }
+    }
+
+    private fun onFechaChanged(fecha: String) {
+        _uiState.update {
+            it.copy(
+                fecha = fecha,
+                errorFecha = if (fecha.isEmpty()) "Fecha obligatoria." else ""
+            )
+        }
+    }
+
+    private fun onClienteChanged(cliente: String) {
+        _uiState.update {
+            it.copy(
+                cliente = cliente,
+                errorCliente = if (_uiState.value.cliente.isBlank() || !Regex("^[a-zA-Z]+$")
+                        .matches(_uiState.value.cliente))
+                    "Cliente obligatorio y solo debe contener letras." else ""
+            )
+        }
+    }
+
+    private fun onAsuntoChanged(asunto: String) {
+        _uiState.update {
+            it.copy(
+                asunto = asunto,
+                errorAsunto = if (asunto.isEmpty()) "Asunto obligatorio." else ""
+            )
+        }
+    }
+
+    private fun onDescripcionChanged(descripcion: String) {
+        _uiState.update {
+            it.copy(
+                descripcion = descripcion,
+                errorDescripcion = if (descripcion.isEmpty()) "Descripción obligatoria." else ""
+            )
+        }
+    }
+
+    private fun validateInput(): Boolean {
+        var valid = true
+        if (_uiState.value.fecha.isEmpty()) {
+            _uiState.update { it.copy(errorFecha = "Fecha obligatoria.") }
+            valid = false
+        }
+        if (_uiState.value.cliente.isBlank() || !Regex("^[a-zA-Z]+$").matches(_uiState.value.cliente)) {
+            _uiState.update { it.copy(errorCliente = "Cliente obligatorio y solo debe contener letras.") }
+            valid = false
+        }
+        if (_uiState.value.asunto.isEmpty()) {
+            _uiState.update { it.copy(errorAsunto = "Asunto obligatorio.") }
+            valid = false
+        }
+        if (_uiState.value.descripcion.isEmpty()) {
+            _uiState.update { it.copy(errorDescripcion = "Descripción obligatoria.") }
+            valid = false
+        }
+        if (_uiState.value.prioridadId == null) {
+            _uiState.update { it.copy(errorPrioridad = "Prioridad obligatoria.") }
+            valid = false
+        }
+        return valid
+    }
+}
+
+
+fun TicketUiState.toEntity(): TicketEntity {
+    return TicketEntity(
+        TicketId = ticketId,
+        fecha = fecha,
+        PrioridadId = prioridadId,
+        Cliente = cliente,
+        Asunto = asunto,
+        Descripcion = descripcion
     )
 }

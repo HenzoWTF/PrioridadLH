0fbcf2133a694d76509515d4ff58e19a54664a0a
HenzoWTF <Urenahenzo@gmail.com>
2024-10-05 11:22:08 -0400

    Implementar retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,10 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit2: 2.9.0")
+    implementation("com.squareup.moshi:moshi: 1.15.0")
+    implementation("com.squareup.retrofit2:converter-moshi: 2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor: 4.11.0")
 }

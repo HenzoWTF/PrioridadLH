f764c7470fe9491c8e3cd1aba26b2a88129c936e
HenzoWTF <Urenahenzo@gmail.com>
2024-10-07 22:16:22 -0400

    Agrega API en Prioridades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,8 +91,8 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     //retrofit
-    implementation("com.squareup.retrofit2:retrofit2: 2.9.0")
-    implementation("com.squareup.moshi:moshi: 1.15.0")
-    implementation("com.squareup.retrofit2:converter-moshi: 2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor: 4.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
--- app/src/main/java/edu/ucne/prioridadlh/data/di/AppModule.kt
+++ app/src/main/java/edu/ucne/prioridadlh/data/di/AppModule.kt
@@ -2,18 +2,54 @@ package edu.ucne.prioridadlh.data.di
 
 import android.content.Context
 import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import edu.ucne.prioridadlh.data.local.database.PrioridadesDb
+import edu.ucne.prioridadlh.data.Remote.API.PrioridadesApi
+import edu.ucne.prioridadlh.data.Remote.API.TicketApi
 import edu.ucne.prioridadlh.data.local.dao.PrioridadesDao
+import edu.ucne.prioridadlh.data.local.database.PrioridadesDb
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
-@Module
+
 @InstallIn(SingletonComponent::class)
+@Module
 object AppModule {
+
+    const val BASE_URL = "https://aplicada2-api-h3bvdff3fxbqe2cv.eastus2-01.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesTicket(moshi: Moshi): TicketApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TicketApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesPrioridadesApi(moshi: Moshi): PrioridadesApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(PrioridadesApi::class.java)
+    }
 
     @Provides
     @Singleton
